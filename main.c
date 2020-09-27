3541229d16e0566da067caca5561d0d9abfe0a94
MrLupi <MrLupi@example.com>
2020-09-27 15:36:47 +0200

    Corrected unsaved main.c

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
- 
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "renderer.h"
+
+
+char *progname;
+
+
+int main( int argc, char** argv )
+{
+    progname = argv[0];
+    
+    
+    return EXIT_SUCCESS;
+}
