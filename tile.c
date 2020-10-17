df7754ab28c8c20881e2fb63aa983ef73f5cbafd
MrLupi <MrLupi@example.com>
2020-10-17 16:36:01 +0200

    Extended new tile system further

--- tile.c
+++ tile.c
@@ -39,6 +39,11 @@ void tcSetTileSquareStr( tcTileSquare_t *square, const char *string )
     square->backgroundColor = string[2];
 }
 
+void tcSetOptionsTileSquare( tcTileSquare_t *square, uint8_t options )
+{
+    square->options = options;
+}
+
 
 tcTile_t *tcCreateTile()
 {
@@ -58,13 +63,7 @@ void tcAddTileSquareToTile( tcTile_t *tile, tcTileSquare_t *square )
     }
 }
 
-void tcAddTileSquaresToTile( tcTile_t *tile, tcTileSquare_t **squares )
-{
-    for( int pos = 0; pos < 4; pos++ )
-    {
-        tile->squares[pos] = squares[pos];
-    }
-}
+
 
 void tcDestroyTile( tcTile_t **tile )
 {
@@ -81,7 +80,41 @@ tcUsableTile_t *tcCreateUsableTile()
 {
     tcUsableTile_t *entity;
     entity = ( tcUsableTile_t* ) balloc ( sizeof( tcUsableTile_t ) );
+
+    entity->allocated = 5;
+    entity->tiles = ( tcTile_t ** ) balloc( sizeof ( tcTile_t *) * entity->allocated );
+
     return entity;
+}
+
+void tcIncreaseUsableTileSize( tcUsableTile_t *tile )
+{
+    tile->allocated += 5;
+    tile->tiles = ( tcTile_t ** ) realloc( tile->tiles, sizeof ( tcTile_t *) * tile->allocated );
+}
+
+void tcAddTileToUsableTile( tcUsableTile_t *usableTile, tcTile_t *tile )
+{
+    if ( usableTile->allocated <= usableTile->numberOfTiles )
+    {
+        tcIncreaseUsableTileSize( usableTile );
+    }
+    usableTile->tiles[usableTile->numberOfTiles++] = tile;
+
+    if ( usableTile->numberOfTiles > 1 )
+    {
+        tcSetOptionUsableTile( usableTile, TC_USABLE_TILE_OPTION_ANIMATED );
+    }
+}
+
+void tcSetOptionUsableTile( tcUsableTile_t *tile, uint8_t options )
+{
+    tile->options |= options;
+}
+
+void tcSetOptionUsableTile( tcUsableTile_t *tile, uint8_t options )
+{
+    tile->options &= ~options;
 }
 
 void tcDestroyUsableTile( tcUsableTile_t **tile )
@@ -95,8 +128,64 @@ tcTileStore_t *tcCreateTileStore()
 {
     tcTileStore_t *entity;
     entity = ( tcTileStore_t* ) balloc ( sizeof( tcTileStore_t ) );
+    
+    entity->allocatedTileSquares = 5;
+    entity->tileSquares = ( tcTileSquare_t ** ) balloc ( sizeof( tcTileSquare_t * ) * entity->allocatedTileSquares );
+
+    entity->allocatedTiles = 5;
+    entity->tiles = ( tcTile_t ** ) balloc ( sizeof( tcTile_t * ) * entity->allocatedTiles );
+
+    entity->allocatedUsableTiles = 5;
+    entity->usableTiles = ( tcUsableTile_t ** ) balloc ( sizeof( tcUsableTile_t * ) * entity->allocatedUsableTiles );
+
     return entity;
 }
+
+void tcTileStoreIncreaseTileSquareSize( tcTileStore_t *store )
+{
+    store->allocatedTileSquares += 5;
+    store->tileSquares = ( tcTileSquare_t ** ) realloc( store->tileSquares, sizeof ( tcTileSquare_t *) * store->allocatedTileSquares );
+}
+
+void tcTileStoreIncreaseTileSize( tcTileStore_t *store )
+{
+    store->allocatedTiles += 5;
+    store->tiles = ( tcTile_t ** ) realloc( store->tiles, sizeof ( tcTile_t *) * store->allocatedTiles );
+}
+
+void tcTileStoreIncreaseUsableTileSize( tcTileStore_t *store )
+{
+    store->allocatedTileSquares += 5;
+    store->usableTiles = ( tcUsableTile_t ** ) realloc( store->usableTiles, sizeof ( tcUsableTile_t *) * store->allocatedTileSquares );
+}
+
+void tcTileStoreAddTileSquare( tcTileStore_t *store, tcTileSquare_t *square )
+{
+    if ( store->allocatedTileSquares <= store->numberOfTileSquares )
+    {
+        tcTileStoreIncreaseTileSquareSize( store );
+    }
+    store->tileSquares[store->numberOfTileSquares++] = square;
+}
+
+void tcTileStoreAddTile( tcTileStore_t *store, tcTile_t *tile )
+{
+    if ( store->allocatedTiles <= store->numberOfTiles )
+    {
+        tcTileStoreIncreaseTileSize( store );
+    }
+    store->tiles[store->numberOfTiles++] = tile;
+}
+
+void tcTileStoreAddUsableTile( tcTileStore_t *store, tcUsableTile_t *usableTile )
+{
+    if ( store->allocatedUsableTiles <= store->numberOfUsableTiles )
+    {
+        tcTileStoreIncreaseUsableTileSize( store );
+    }
+    store->usableTiles[store->numberOfUsableTiles++] = usableTile;
+}
+
 
 void tcDestroyTileStore( tcTileStore_t **store )
 {
--- tile.h
+++ tile.h
@@ -2,19 +2,20 @@
 #define TC_TILE_H_INCLUDE
 #include <inttypes.h>
 
-
 typedef struct __tc_tile_square
 {
     uint8_t character;
     uint8_t color;
     uint8_t backgroundColor;
-    uint8_t reserved;
+    uint8_t options;
 } tcTileSquare_t;
 
 tcTileSquare_t *tcCreateTileSquare();
 void tcDestroyTileSquare( tcTileSquare_t **square );
 void tcSetTileSquare( tcTileSquare_t *square, uint8_t character, uint8_t color, uint8_t backgroundColor );
 void tcSetTileSquareStr( tcTileSquare_t *square, const char *string );
+void tcSetOptionsTileSquare( tcTileSquare_t *square, uint8_t options );
+
 
 typedef struct __tc_tile
 {
@@ -23,29 +24,46 @@ typedef struct __tc_tile
 
 tcTile_t *tcCreateTile();
 void tcAddTileSquareToTile( tcTile_t *tile, tcTileSquare_t *square );
-void tcAddTileSquaresToTile( tcTile_t *tile, tcTileSquare_t **squares );
 void tcDestroyTile( tcTile_t **tile );
 
+#define TC_USABLE_TILE_OPTION_ANIMATED 1
 
 typedef struct __tc_usable_tile
 {
     tcTile_t **tiles;
     int16_t numberOfTiles;
+    int16_t allocated;    
     int16_t options;
 } tcUsableTile_t;
 
 tcUsableTile_t *tcCreateUsableTile();
+void tcIncreaseUsableTileSize( tcUsableTile_t *tile );
+void tcAddTileToUsableTile( tcUsableTile_t *usableTile, tcTile_t *tile );
+void tcSetOptionUsableTile( tcUsableTile_t *tile, uint8_t options );
+void tcResetOptionUsableTile( tcUsableTile_t *tile, uint8_t options );
 void tcDestroyUsableTile( tcUsableTile_t **tile );
 
 
 typedef struct __tc_tile_store
 {
     tcTileSquare_t **tileSquares;
+    uint16_t allocatedTileSquares;
+    uint16_t numberOfTileSquares;
     tcTile_t **tiles;
-    tcUsableTile_t **uTiles;
+    uint16_t allocatedTiles;
+    uint16_t numberOfTiles;
+    tcUsableTile_t **usableTiles;
+    uint16_t allocatedUsableTiles;
+    uint16_t numberOfUsableTiles;
 } tcTileStore_t;
 
 tcTileStore_t *tcCreateTileStore();
+void tcTileStoreIncreaseTileSquareSize( tcTileStore_t *store );
+void tcTileStoreIncreaseTileSize( tcTileStore_t *store );
+void tcTileStoreIncreaseUsableTileSize( tcTileStore_t *store );
+void tcTileStoreAddTileSquare( tcTileStore_t *store, tcTileSquare_t *square );
+void tcTileStoreAddTileSquare( tcTileStore_t *store, tcTileSquare_t *square );
+void tcTileStoreAddTile( tcTileStore_t *store, tcTile_t *tile );
 void tcDestroyTileStore( tcTileStore_t **store );
 
 #endif // TC_TILE_H_INCLUDE
